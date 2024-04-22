4102b923992daf03f62992b1ccb9ba1a0fd1eecd
fardmaster30 <denial2143@gmail.com>
2024-04-22 08:14:09 -0400

    new awessome stuff
    cool epic awesome things on 4/22 firs period

--- Heavy Impact - CaseOh's Furious Rampage/Assets/EnemySpawner.cs
+++ Heavy Impact - CaseOh's Furious Rampage/Assets/EnemySpawner.cs
@@ -15,17 +15,27 @@ public class EnemySpawner : MonoBehaviour
     }
     private void Update()
     {
-        spawnTime = spawnTime / 1 - (Time.deltaTime * spawnInclineRate);
+        spawnTime = spawnTime * (1 - (Time.deltaTime * spawnInclineRate));
     }
     IEnumerator EnemySpawn()
     {
         yield return new WaitForSeconds(spawnTime);
-        float randX = Random.Range(-23, 23);
-        float randY = Random.Range(-23, 23);
-        Vector3 randPos = new Vector3(randX, randY,0);
         
-        GameObject alien1 = Instantiate(alien, randPos,Quaternion.identity);
+        GameObject alien1 = Instantiate(alien, GenerateVectorOutsideCamera(),Quaternion.identity);
         alien1.GetComponent<AIDestinationSetter>().target = GameObject.Find("Caseoh").transform;
         StartCoroutine(EnemySpawn());
+    }
+    Vector3 GenerateVectorOutsideCamera()
+    {
+        float camLimit1 = 13;
+        float camLimit2 = 7;
+        float randX = Random.Range(-23, 23);
+        float randY = Random.Range(-23, 23);
+        Vector3 randPos = new Vector3(randX, randY, 0);
+        if (randPos.x > camLimit1+ Camera.main.transform.position.x || randPos.x < -camLimit1 + Camera.main.transform.position.x || randPos.y > camLimit2+ Camera.main.transform.position.y || randPos.y < -camLimit2 + Camera.main.transform.position.y)
+        {
+            return randPos;
+        }
+        return GenerateVectorOutsideCamera();
     }
 }
