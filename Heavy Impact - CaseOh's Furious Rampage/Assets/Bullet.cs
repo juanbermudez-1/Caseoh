ff133df09ffc9a0e3df4375822f2bcb9e2bfee27
fardmaster30 <denial2143@gmail.com>
2024-04-23 07:32:16 -0400

    changes

--- Heavy Impact - CaseOh's Furious Rampage/Assets/Bullet.cs
+++ Heavy Impact - CaseOh's Furious Rampage/Assets/Bullet.cs
@@ -5,14 +5,9 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] float bulletSpeed;
-<<<<<<< Updated upstream:Heavy Impact - CaseOh's Furious Rampage/Assets/Bullet.cs
     [SerializeField] int enemyLayer; 
-=======
-    private int thisHealth;
->>>>>>> Stashed changes:Heavy Impact - CaseOh's Furious Rampage/Assets/Scripts/Bullet.cs
     private void Start()
     {
-        thisHealth = 3;
         Debug.Log(this.transform.rotation.z);
         Destroy(this.gameObject, 3);
     }
@@ -22,28 +17,15 @@ public class Bullet : MonoBehaviour
         float zRot = (this.transform.rotation.eulerAngles.z+90)*Mathf.PI/180;
         float speed = bulletSpeed * Time.deltaTime;
         this.transform.position += new Vector3(Mathf.Cos(zRot),Mathf.Sin(zRot),0).normalized*speed;
-        if (thisHealth <= 0) Destroy(this.gameObject);
     }
-<<<<<<< Updated upstream:Heavy Impact - CaseOh's Furious Rampage/Assets/Bullet.cs
     private void OnCollisionEnter2D(Collision2D collision)
-=======
-    private void OnTriggerEnter2D(Collider2D collision)
->>>>>>> Stashed changes:Heavy Impact - CaseOh's Furious Rampage/Assets/Scripts/Bullet.cs
     {
         if(collision.collider.gameObject.layer == 8)
         {
             Destroy(collision.gameObject);
-            if (Gun.gunMode == 2)
-            {
-                --thisHealth;
-            }
-            else Destroy(this.gameObject);
+            Destroy(this.gameObject);
         }
-<<<<<<< Updated upstream:Heavy Impact - CaseOh's Furious Rampage/Assets/Bullet.cs
         else if(collision.collider.gameObject.layer == 7)
-=======
-        if (collision.gameObject.layer == 7)
->>>>>>> Stashed changes:Heavy Impact - CaseOh's Furious Rampage/Assets/Scripts/Bullet.cs
         {
             Destroy(this.gameObject);
         }
