0b50add782bd704aa34fb331db2b51caca546f1b
juanbermudez-1 <149680394+juanbermudez-1@users.noreply.github.com>
2024-05-14 08:14:11 -0400

    added button

---
+++ Heavy Impact - CaseOh's Furious Rampage/Assets/Scripts/Start menu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Startmenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+}
+   
